4150df253d2085b82cfa44666667a1435f720b30
Krista Blachian <KristaBlachian@outlook.com>
2018-06-20 23:50:06 -0400

    application complete

--- app/src/main/java/com/theironyard/seekbar/checkbox/MainActivity.java
+++ app/src/main/java/com/theironyard/seekbar/checkbox/MainActivity.java
@@ -2,12 +2,48 @@ package com.theironyard.seekbar.checkbox;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CheckBox momCheckBox;
+    private CheckBox dadCheckBox;
+    private CheckBox grandpaCheckBox;
+
+    private Button showButton;
+    private TextView showTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        momCheckBox = (CheckBox) findViewById(R.id.momCheckBoxID);
+        dadCheckBox = (CheckBox) findViewById(R.id.dadCheckBoxID);
+        grandpaCheckBox = (CheckBox) findViewById(R.id.grandpaCheckBoxID);
+
+        showTextView = (TextView) findViewById(R.id.resultID);
+
+        showButton = (Button) findViewById(R.id.showButtonID);
+        showButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                StringBuilder stringBuilder = new StringBuilder();
+                stringBuilder.append(momCheckBox.getText().toString() + " status is: " +
+                momCheckBox.isChecked() + "\n");
+                stringBuilder.append(dadCheckBox.getText().toString() + " status is: " +
+                dadCheckBox.isChecked() + "\n");
+                stringBuilder.append(grandpaCheckBox.getText().toString() + " status is: "
+                + grandpaCheckBox.isChecked() + "\n");
+
+                showTextView.setText(stringBuilder);
+
+            }
+        });
     }
 }
